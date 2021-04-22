c651aef43c977446cf0352f0f41b8e315665c2c6
Christian Smucker <chrissmucker94@gmail.com>
2021-04-21 22:14:41 -0600

    Add airport

---
+++ models/modelA.js
@@ -0,0 +1,10 @@
+var austinBergstrom = {
+ var n: "Austin",
+ var x: "bbq",
+ var zipCode: 78719,
+ var areaCode: 512,
+ var isAirport: true,
+ var hasPlanes: true,
+ var airLines: ["Delta", "American", "Southwest"],
+ var restaurants: ["Starbucks", "Flyrite", "Stubbs"]
+}
