132b4aa21418ffb7f44fbd07f9f5c1462d0b7dfe
Christian Smucker <chrissmucker94@gmail.com>
2021-04-21 22:03:47 -0600

    Add variables

---
+++ variables.js
@@ -0,0 +1,9 @@
+var famousPerson = "Brad Pitt";
+var isActive = true;
+famousPerson = "George Clooney";
+var num3 = 5;
+var num9 = 3;
+var sum = num3 + num9;
+var animals = ["lions", "tigers", "bears"];
+animals.shift();
+var n = famousPerson.length;
