ae5c392925ffd201cc6eeb98636c3818f28f7bdb
Christian Smucker <chrissmucker94@gmail.com>
2021-04-21 22:21:09 -0600

    Add vehicle

---
+++ models/modelB.js
@@ -0,0 +1,10 @@
+var teslaModelX = {
+ n: "fast",
+ x: "Elon Musk",
+ mPH: 340,
+ horsePower: 1020,
+ falconDoors: true,
+ isCool: true,
+ seatsOptions: [5, 6, 7],
+ colorOptions: ["white", "black", "grey", "blue", "red"]
+}
